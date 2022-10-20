34995ab675784e51a49d6db536ea478aba0d8792
Luisa <Luisa@DESKTOP-2U7MAPV>
2022-10-20 23:35:34 +0200

    :/

--- EjerciciosT2-LuisaCardozo/src/ejercicios/Ejercicio07.java
+++ EjerciciosT2-LuisaCardozo/src/ejercicios/Ejercicio07.java
@@ -1,11 +1,40 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio07 {
 /*Dada una hora por teclado, (horas, minutos y segundos) 
- * cirealiza un algoritmo que muestre la hora después de incrementarle un segundo.*/
+ * realiza un algoritmo que muestre la hora después de incrementarle un segundo.*/
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//declaramos las variables
+		int horaU, minutosU, segundosU;//en hora user es donde el usuario va a introducir la hora
+		
+		//iniciamos el scanner
+		Scanner lee = new Scanner(System.in);
+		
+		System.out.println("Por favor introduzca la hora: ");//guardamos la hora que va a introducir el usuario en la variable horaU
+		horaU = lee.nextInt();
+		
+		//guardamos los minutos que va a introducir el usuario en la variable minutosU
+		System.out.println("Por favor introduzca los minutos: ");
+		minutosU = lee.nextInt();
+		
+		//guardamos los minutos que va a introducir el usuario en la variable segundosU
+		System.out.println("Por favor introduzca los segundos: ");
+		segundosU = lee.nextInt();
+		
+		if (horaU>23 && horaU<25) {
+			System.out.println("00");
+		}
+		else if (minutosU>59 && minutosU<60) {
+			System.out.println("00");
+		}
+		
+		else if(segundosU>59 && segundosU<60) {
+			System.out.println("00");
+		}
+	}
 	}
 
-}
---
+++ EjerciciosT2-LuisaCardozo/src/ejercicios/Ejercicio08.java
@@ -0,0 +1,46 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio08 {
+/*Determinar el precio de un billete de tren, conociendo la distancia a recorrer, 
+ * y sabiendo que si el número de días de estancia es superior a 7 
+ * y la distancia superior a 800 km el billete tiene una reducción del 30%.
+ *  El precio por kilómetro es de 2,5€.
+ *  La distancia a recorrer y el número de días de estancia los debes solicitar al usuario por teclado.*/
+	public static void main(String[] args) {
+		//establecemos las variables 
+		int estanciaU, distanciaRecorrida; //en estas variables guardamos los dias de estancia y la distancia a recorrer
+		double billeteUser;
+		final double PRECIOKM = 2.5;
+		final double DESCUENTO = 0.3;
+		double descTotal;
+		
+		//iniciamos el scanner
+		Scanner lee = new Scanner(System.in);
+		
+		//Le solicitamos al usuario la distancia a recorrer y el n de dias de estancia
+		System.out.println("Por favor introduzca la distancia a recorrer: ");
+		distanciaRecorrida = lee.nextInt();
+		
+		System.out.println("Por favor introduzca el numero de dias de estancia: ");
+		estanciaU = lee.nextInt();
+		
+		//en billete user vamos a guardar la multiplicacion de la distancia por los 2,5
+		billeteUser = PRECIOKM * distanciaRecorrida; 
+		
+		//en desTotal vamos a guardar el precio del billete menos el descuento
+		descTotal = billeteUser - (billeteUser* DESCUENTO);
+		
+		//establecemos un if donde si la distancia es mayor a 800 y la estancia es mayor a 7, entonces se aplicará el descTotal
+		if (estanciaU > 7 && distanciaRecorrida > 800) {
+			System.out.println("El precio final de su billete con el descuento del 30% aplicado es : " + (descTotal) + "euros");
+		}
+		else { //en caso contrario solo va a imprimir el precio del billete
+			System.out.println("El precio del billete es: " + billeteUser + " euros.");
+		}
+	//Cerramos el scanner
+		lee.close();
+	}
+
+}
